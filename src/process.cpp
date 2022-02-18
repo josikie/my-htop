f21e08ceff181f040f683057be48d43af71f75ba
Josikie <kiejosi12@gmail.com>
2022-02-18 01:30:13 +0000

    Completing process

--- src/process.cpp
+++ src/process.cpp
@@ -11,32 +11,42 @@ using std::string;
 using std::to_string;
 using std::vector;
 
+
 // TODO: Return this process's ID
 int Process::Pid() { return pid_;}
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { 
+float Process::CpuUtilization() const{ 
   long uptime = LinuxParser::UpTime();
   float hertz = sysconf(_SC_CLK_TCK);
   string line;
-  int total_time;
+  string value;
+  int total_time, utime, stime, cutime, cstime, starttime;
   int seconds;
   float cpu_usage;
-  vector<string> number;
   std::ifstream stream(LinuxParser::kProcDirectory + std::to_string(pid_) + LinuxParser::kStatFilename);
   if(stream.is_open()){
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> number[0] >> number[1] >> number[2] >> number[3] >> number[4] >> number[5] 
-    >> number[6] >> number[7] >> number[8] >> number[9] >> number[10] >> number[11] >> number[12]
-    >> number[13] >> number[14] >> number[15] >> number[16] >> number[17] >> number[18] >> number[19]
-    >> number[20] >> number[21];
-    total_time = stoi(number[13]) + stoi(number[14]);
-    total_time = total_time + stoi(number[15]) + stoi(number[16]);
-    seconds = uptime - (stoi(number[21]) / hertz);
+    for(int i = 1; i <= 22; i++){
+      linestream >> value;
+      if(i == 14){
+        utime = stoi(value);
+      }else if(i == 15){
+        stime = stoi(value);
+      }else if(i == 16){
+        cutime = stoi(value);
+      }else if(i == 17){
+        cstime = stoi(value);
+      }else if(i == 22){
+        starttime = stoi(value);
+      }
+    }
+    total_time = utime + stime;
+    total_time = total_time + cutime + cstime;
+    seconds = uptime - starttime / hertz;
     cpu_usage = 100 * ((total_time / hertz) / seconds);
   }
-  cpuUtilization = cpu_usage;
   return cpu_usage;
 }
 
@@ -63,5 +73,5 @@ long int Process::UpTime() {
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const { 
-  return this->cpuUtilization < a.cpuUtilization;
+  return a.CpuUtilization() < this->CpuUtilization();
 }
